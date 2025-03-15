a3de9f53495b19a24bfd86a2410628e85992e577
Shishir Lakkadi <shishir.lakkadi@gmail.com>
2025-03-15 20:42:17 +0000

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,18 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware()
+// Define public routes that don't require authentication
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/private-route(.*)'])
 
+// Apply middleware
+export default clerkMiddleware(async (auth, req) => {
+    if (isProtectedRoute(req)) await auth.protect()
+  })
+
+// Configure the matcher to specify which routes the middleware applies to
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Apply middleware to all routes except those specified as public
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    '/(api|trpc)(.*)', // Always run for API routes
   ],
-}
+};
